e5a6ec2a9e3f07a6ef84dfdc71866a55dda15ff9
Mojo <muzamil.tahir1738@gmail.com>
2025-03-01 16:12:35 +0500

    updating

---
+++ app.capstoneproject/components/Nav.js
@@ -0,0 +1,11 @@
+<nav>
+    <img src= "Logo.svg" alt="Logo" />
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="littlelemon.com/about">About</a></li>
+        <li><a href="littlelemon.com/menu">Menu</a></li>
+        <li><a href="littlelemon.com/reservation">Reservation</a></li>
+        <li><a href="littlelemon.com/order-online">Order Online</a></li>
+        <li><a href="littlelemon.com/login">Login</a></li>
+    </ul>
+</nav>
--- app.capstoneproject/src/App.js
+++ app.capstoneproject/src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <Header></Header>
+     <Nav></Nav>
+    <Main></Main>
+    <Footer></Footer>
   );
 }
 
