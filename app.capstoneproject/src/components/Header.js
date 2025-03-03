d305bbc5253851adc59a30aa923f972ee578cf89
Mojo <muzamil.tahir1738@gmail.com>
2025-03-03 08:26:57 +0500

    progress

--- app.capstoneproject/src/components/Header.js
+++ app.capstoneproject/src/components/Header.js
@@ -1,11 +1,11 @@
 function Header() {
     return (
         <header class = 'heading'>
-           <h1 class = 'title'>Little Lemon</h1>
-            <h2 class ='subHeading'>Chicago</h2>
-            <img class = 'headingImg' src = {require ('./restauranfood.jpg')} alt = 'restaurant food' />
-            <p class = 'description'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-            <a href = "littlelemon.com/reservation" class = "reserveButton">Reserve a Table</a>
+           <div class = 'title'><h1>Little Lemon</h1></div>
+           <div class = 'headingImg'><img src = {require ('./restauranfood.jpg')} alt = 'restaurant food' width='240px'/></div>
+            <div class = 'subHeading'><h2>Chicago</h2></div>
+            <div class = 'description'><p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p></div>
+            <div class = 'button'><a href = "littlelemon.com/reservation" class = 'reserve'>Reserve a Table</a></div>
         </header>
     );
 }
--- app.capstoneproject/src/components/Main.js
+++ app.capstoneproject/src/components/Main.js
@@ -1,6 +1,36 @@
 function Main() {
     return (
         <main className="main">
+            <div class = 'specialsHeading'>
+            <h3>This weeks specials</h3>
+            <a href = 'littlelemon.com/menu'>Online Menu</a>
+            </div>
+            <div class= 'menuCard'>
+            <div class = 'greekSalad'>
+            <img src = {require('./greek salad.jpg')} width = '100px' alt = "greeksalad" class = 'greekSaladImg' />
+            <h4>Greek salad</h4>
+            <p>$12.99</p>
+            <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+            <a href = 'l'>Order a delivery </a>
+            <img url= {require('./bike.svg')} alt= 'bike'/>
+            </div>
+            <div class="bruchetta">
+            <img src ={require('./bruchetta.svg').default} alt = 'bruchetta' class= 'bruchettaImg' />
+            <h4>Bruchetta</h4>
+            <p>$5.99</p>
+            <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+            <a href = 'l'>Order a delivery </a>
+            <img url= {require('./bike.svg').default} alt= 'bike'/>
+            </div>
+            <div class = "lemonDessert">
+            <img src ={require('./lemon dessert.jpg')} alt= 'lemon' class="lemmonImg"/>
+            <h4>Lemon Dessert</h4>
+            <p>$5.00</p>
+            <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+            <a href = 'l'>Order a delivery </a>
+            <img url= {require('./bike.svg')} alt= 'bike'/>
+            </div>
+            </div>
         </main>
     )
 }
